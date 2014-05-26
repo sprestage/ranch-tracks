7833e5ac6e6100b3c66a5fce06ec2e4c3d06196a
Susan Prestage <susan.prestage@gmail.com>
2014-05-26 16:55:12 -0700

    Tweak things for Travis to work.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,8 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Tracksapp::Application.config.secret_key_base = SITE_CONFIG["secret_token"] || SecureRandom.hex(64)
+Tracksapp::Application.config.secret_key_base = if Rails.env.development? || Rails.env.test?
+    ('x' * 30) # meets minimum requirement of 30 chars long
+  else
+    ENV['SECRET_TOKEN']
+  end
